7634a21f6ffe2fa2fec66169c8d4d3ed5b0104f2
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-08-12 23:54:22 +0430

    Hello World

---
+++ javaScript.js
@@ -0,0 +1,12 @@
+/*jslint browser:true*/
+/*jslint devel:true*/
+/*global
+    window, $
+*/
+
+function printHello() {
+    document.getElementById("pHello").innerHTML = "Hello World!";
+}
+function btnHello() {
+    printHello();
+}
